6703cac8677baf1b4cca1fcc7a3f971868ae3bc5
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-01-30 16:43:52 +0600

    add #5 dqatatype

---
+++ #5 datatype.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // char,int , float, double,bool 
+    char c = 'Nirob';
+    int a = 1;
+    double b = 2.1;
+    a = 4.5;
+    bool d = 0;
+    bool e = -111;
+    cout << c << " " << a << " " << b << " " << d << " " << e << endl;
+    cout << e;
+}
