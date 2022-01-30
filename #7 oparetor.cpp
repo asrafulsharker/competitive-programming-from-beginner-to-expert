0b0a7de46b774feb05ce3c1fd44e391a2f4bae1d
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-01-30 17:03:28 +0600

    add #7 oparetor

---
+++ #7 oparetor.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // multiplication 
+  cout << 2*3 << endl;
+//   modulus 
+  cout << 20%7 << endl;
+//   divition 
+cout << 20/7 << endl;
+int a =5 ;
+a = a+2;
+cout << a << endl;
+a++;
+cout << a << endl;
+++a;
+cout << a << endl;
+a--;
+cout << a << endl;
+--a;
+cout << a << endl;
+
+cout << a++ << endl;
+cout << a << endl;
+cout << ++a << endl;
+cout << a << endl;
+}
