8d690e4f99cb3f858b3e1466cebf4059463e9125
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-01-31 20:04:46 +0600

    add #11 and #12

--- #11 datatype overflow.cpp
+++ #11 datatype overflow.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()(
-    
-)
+int main(){
+    // int, char,long,long long int, float, double 
+
+    long long int a = 100000;
+    long long int b = 100000;
+    long long int c = a*b;
+    cout << c << endl;
+
+    double x  = 100000;
+    double y= 100000;
+    double z = x*y;
+    cout << z << endl;
+
+    cout << fixed << z << endl;
+    cout << fixed << setprecision(0) << z << endl;
+    double l = 10;
+    l = 1e24;
+    cout << fixed << l << endl;
+}
---
+++ #12 question.cpp
@@ -0,0 +1,39 @@
+// Given rectangle of length l and b. Print area of rectangle .
+
+
+// Constraints:
+// 1 <= l <= 10^0
+// 1 <= b <= 10^9
+
+// Input format:
+// 2 Space sperated integer l an b
+
+// Output format:
+// Single Number which is area of rectengle 
+
+// simple input:
+// 3 4 
+
+// Sample Output:
+
+// 12
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long a, b;
+    cin >> a >>b;
+    cout << a*b <<endl;
+}
+
+ // or  ==================================================
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int x,y;
+//     cin >> x >>y;
+//     cout << x * 1LL *b << endl;
+// }
