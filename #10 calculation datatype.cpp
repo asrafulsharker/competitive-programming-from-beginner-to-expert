0979e4fba96748a375ae97a080bdf73ebb17beae
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-01-31 19:15:05 +0600

    add #10

--- #10 calculation datatype.cpp
+++ #10 calculation datatype.cpp
@@ -5,5 +5,7 @@ int main(){
     cout << 7 / 2.0 << endl;
     cout << 'c' + 1 << endl;
     int a  =3/2;
-    cout << a;
+    cout <<  a << endl;
+    cout << 7/2*3 << endl;
+    cout << 3*7/2 << endl;
 }
