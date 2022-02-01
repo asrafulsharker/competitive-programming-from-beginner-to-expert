075f46493c55f96dcaac34651f03067820bd47ed
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-02-01 12:42:15 +0600

    add13

---
+++ #13 multiple condition.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a =1;
+    int b = 2;
+    int c = 3;
+    cout << ((b<c) && (a>c)) << endl;
+     cout << ((b<c) || (a>c)) << endl;
+}
