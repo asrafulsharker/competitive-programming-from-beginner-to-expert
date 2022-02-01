eaa6029561f509a3ead6ee76d19ac1669fff6db1
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-02-01 12:52:54 +0600

    add #14 scope

---
+++ #14 scope.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int x;
+
+int main(){
+    int n;
+    cin >> n;
+    {
+        x =3;
+        cout << x << endl;
+    }
+    x =2;
+    cout << x;
+}
