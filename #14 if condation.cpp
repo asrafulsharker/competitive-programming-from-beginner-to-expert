c29c98d0b24ac7e0f6e32d3465288d2170d9daba
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-02-01 12:48:17 +0600

    add #14

---
+++ #14 if condation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n ;
+    cin >> n;
+    if(n%2 == 0){
+        cout << "Yes ";
+    }
+    else if(n/1 == 9){
+        cout << "Yes This is 9";
+    }
+    else{
+        cout << "All are false";
+    }
+}
