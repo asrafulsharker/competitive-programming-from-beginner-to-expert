cbacb8413551bc6db1f0ddefd3da2b3eaef95850
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-02-08 19:20:26 +0600

    add 22

---
+++ #22 check string equal or not.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str = "Nirob";
+    string str2 = "Nirob";
+    string result = str + str2;
+    if (str == str2){
+        cout << "Equal";
+    }
+    else{
+        cout << " Not Equal";
+    }
+}
