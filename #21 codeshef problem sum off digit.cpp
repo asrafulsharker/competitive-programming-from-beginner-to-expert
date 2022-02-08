18027a795542be2fe66d747e69b3559952da96d6
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-02-08 19:07:35 +0600

    add 21#

---
+++ #21 codeshef problem sum off digit.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >>t;
+    while(t--){
+    int n;
+        cin >> n;
+        int digit_sum = 0;
+        while (n>0){
+            int last_digit = n%10;
+            digit_sum = last_digit+digit_sum;
+            n = n/10;
+        }
+        cout << digit_sum <<endl;
+    }
+  
+}
