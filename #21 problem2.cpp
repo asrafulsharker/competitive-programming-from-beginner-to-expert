d3d5055efb76902ba1ea867ab4fafb3626a97f6e
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-02-08 18:50:20 +0600

    add 18 , 19,20,21

---
+++ #21 problem2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    while (true)
+    {
+        int x,y,i;
+        cin >>  x >> y;
+      for (i = x; i <= y/2; ++i) {
+            if (x % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    
+    
+}
